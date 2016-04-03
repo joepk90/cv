c59df2887eafe6a4a5d5e5003994fc60ddc4c5ae
Joe Park-Kennaby <parkkennaby@joepk.lan>
2016-04-03 23:00:48 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+<link rel="stylesheet" type="text/css" href="jquery.fullPage.css" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/vendor/vendors/jquery.slimscroll.min.js"></script>
+<script type="text/javascript" src="js/vendor/jquery.fullPage.js"></script>
